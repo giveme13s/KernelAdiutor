5cb945ffd720dccf5b30a1767f0b3b1d13c863a8
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-04-07 00:09:23 +0200

    Show app version in the app info
    Change-Id: I7b0798c36a8c8ee40fa94056c44e7fba2cef67d8

--- Android.mk
+++ Android.mk
@@ -4,7 +4,6 @@ include $(CLEAR_VARS)
 LOCAL_MODULE_TAGS := optional
 LOCAL_PACKAGE_NAME := KernelAdiutor
 LOCAL_PRIVILEGED_MODULE := true
-LOCAL_CERTIFICATE := platform
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main)
