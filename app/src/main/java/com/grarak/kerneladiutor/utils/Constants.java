b6090c6a346545d9d2c2acdf19520e352b20be97
Willi Ye <williye97@gmail.com>
2015-04-27 13:33:38 +0200

    Replace ListViews with Recyclerviews This contains ugly hacks I'm too lazy to write a good code
    Conflicts:
    	app/src/main/java/com/grarak/kerneladiutor/utils/Constants.java
    
    Change-Id: I0b24292d243c13bb03be47e5b70a13a2671d64fe

--- app/src/main/java/com/grarak/kerneladiutor/utils/Constants.java
+++ app/src/main/java/com/grarak/kerneladiutor/utils/Constants.java
@@ -16,7 +16,7 @@
 
 package com.grarak.kerneladiutor.utils;
 
-import com.grarak.kerneladiutor.elements.ListAdapter;
+import com.grarak.kerneladiutor.elements.DAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -30,7 +30,7 @@ public interface Constants {
     String VERSION_NAME = "0.8.9.2";
     String PREF_NAME = "prefs";
     String GAMMA_URL = "https://raw.githubusercontent.com/Grarak/KernelAdiutor/master/gamma_profiles.json";
-    List<ListAdapter.ListItem> ITEMS = new ArrayList<>();
+    List<DAdapter.DView> ITEMS = new ArrayList<>();
 
     // Kernel Informations
     String PROC_VERSION = "/proc/version";
