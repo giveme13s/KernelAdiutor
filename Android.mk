5ed8924c7d487bff77af3bb0a0ba34f104651271
fusionjack <dogfight60-fusionjack@yahoo.de>
2015-05-30 12:24:02 +0200

    Don't build KernelAdiutor as a privileged app
    Change-Id: I95e2115f0bffaeda6ab5e91c1f736781029cfae2

--- Android.mk
+++ Android.mk
@@ -3,7 +3,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_PACKAGE_NAME := KernelAdiutor
-LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_SRC_FILES := $(call all-java-files-under, app/src/main)
